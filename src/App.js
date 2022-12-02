cfa4f0141014c795b5acef9b620845baffd6f723
Sura Karthikeya <58618233+abhinit21@users.noreply.github.com>
2022-12-02 16:43:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path="/:product" element={<Product />} />
+      <Route path="compare-products/:product" element={<Product />} />
     </Routes>
   );
 }
