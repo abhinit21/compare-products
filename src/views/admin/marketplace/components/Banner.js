090189502cc77f3176a79249cfd4c7ae4c02d08c
abhinit21 <surak504@gmail.com>
2022-12-05 10:09:38 +0000

    resolve conflict-01

--- src/views/admin/marketplace/components/Banner.js
+++ src/views/admin/marketplace/components/Banner.js
@@ -1,63 +1,63 @@
 import React from "react";
 
 // Chakra imports
-import { Button, Flex, Link, Text } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
-// Module imports
-import {Carousel} from 'react-bootstrap';
+// Other imports
+import { Carousel } from "react-bootstrap";
 
 // Assets
-import banner from "assets/img/nfts/blue.png";
+import banner from "assets/img/nfts/NftBanner1.png";
+import bannerimage from "assets/img/nfts/banner.png";
 
 export default function Banner() {
   // Chakra Color Mode
   return (
     <Flex
-      direction='column'
+      direction="column"
       bgImage={banner}
-      bgSize='cover'
-      py={{ base: "30px", md: "56px" }}
-      px={{ base: "30px", md: "64px" }}
-      borderRadius='30px'>
-<Carousel slide={false}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.ctfassets.net/cnu0m8re1exe/7bvBF6E4WXVLLIsH88lXcC/5a3b7483a82fb30248e7d1c7856be6ec/20-things-color.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.ctfassets.net/cnu0m8re1exe/7bvBF6E4WXVLLIsH88lXcC/5a3b7483a82fb30248e7d1c7856be6ec/20-things-color.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.ctfassets.net/cnu0m8re1exe/7bvBF6E4WXVLLIsH88lXcC/5a3b7483a82fb30248e7d1c7856be6ec/20-things-color.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+      bgSize="cover"
+      py={{ base: "10px", md: "24px" }}
+      px={{ base: "10px", md: "24px" }}
+      borderRadius="30px"
+    >
+      <Carousel className="rounded-5">
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100 rounded-5"
+            src={bannerimage}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100 rounded-5"
+            src={bannerimage}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100 rounded-5"
+            src={bannerimage}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </Flex>
   );
 }
