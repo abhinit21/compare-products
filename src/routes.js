65c8ed525b459b8fc88a99a6b992e767df3f421e
KARTHIKEYA <Surak504@gmail.com>
2022-12-05 12:06:30 +0530

    📃base compare page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 import { Icon } from "@chakra-ui/react";
-import { MdPerson, MdHome, MdLock } from "react-icons/md";
+import { MdPerson, MdHome, MdLock, MdCompare } from "react-icons/md";
 
 // Admin Imports
+import Default from "views/admin/default";
 import NFTMarketplace from "views/admin/marketplace";
 import Profile from "views/admin/profile";
 
@@ -18,6 +19,13 @@ const routes = [
     icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
     component: NFTMarketplace,
     secondary: true,
+  },
+  {
+    name: "Compare",
+    layout: "/admin",
+    path: "/view-prices",
+    icon: <Icon as={MdCompare} width="20px" height="20px" color="inherit" />,
+    component: Default,
   },
   {
     name: "Profile",
